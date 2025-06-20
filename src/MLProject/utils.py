811ec21c4517ac51cbe2b7bfb57f3ba8c0ea72cb
Gaurav <gauravmore0206@gmail.com>
2025-06-20 15:48:22 +0530

    model Training

--- src/MLProject/utils.py
+++ src/MLProject/utils.py
@@ -2,6 +2,8 @@ import os
 import sys
 from src.MLProject.exception import CustomException
 from src.MLProject.logger import logging
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import pandas as pd
 from dotenv import load_dotenv
 import pymysql
@@ -35,6 +37,7 @@ def read_sql_data():
     except Exception as e:
         raise CustomException(e)
     
+    
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -46,3 +49,31 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)    
+
+def evaluate_models(X_train,y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)  # Train Model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
